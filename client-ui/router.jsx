057579f42ac94f0ef9da94e1d4d02354942c8738
William Surya Wijaya <williamsw.gb@gmail.com>
2015-12-29 02:22:27 +0800

    Add client ui apps

---
+++ client-ui/router.jsx
@@ -0,0 +1,38 @@
+// handling /trails
+var trailsRoutes = FlowRouter.group({
+  prefix: '/trails',
+  name: 'trails',
+});
+
+// handling /trails/:username
+var userRoutes = trailsRoutes.group({
+    prefix: "/:username",
+    name: "user"
+});
+
+// handling /trails/:username/:userID
+userRoutes.route('/:trailsID', {
+  action: function(params) {
+    /*
+    Find the trails using the username and trailsID
+    console.log(params);
+    console.log(params.username);
+    console.log(params.trailsID);
+    */
+    //Passing the map options parameter
+    var mapOptions = {
+      lat: 1.3431824453335746,
+      lng: 103.69153617919926,
+      zoom: 15,
+      points: {
+        startPoint:{lat:1.3499800000000002, lng:103.68092999999999},
+        endPoint:{lat:1.33755, lng:103.69686000000002},
+        lookAtPoint:{lat:1.3431824453335746, lng:103.69153617919926}
+      }
+    };
+
+    ReactLayout.render(App, {
+      map: <MyTestMap mapOptions={mapOptions}/>,
+    });
+  }
+});
